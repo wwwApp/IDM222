7911d45900f65ff82fb2a34aabbfb764f17a6309
woo song <ws392@drexel.edu>
2018-03-09 09:55:11 -0500

    Code Update
    HalfCut logo and contact established; margins fixed; toggle-details function established

--- main.js
+++ main.js
@@ -1,55 +1,54 @@
-//function to toggle the visibility of the project div
-const worksTitle = document.querySelectorAll('.worksTitle');
-function toggleWorkItem(event){
+//function to prevent default action for anchor elements
+const anchors = document.querySelectorAll('a');
+for(anchor of anchors){
+    anchor.addEventListener('click', function(){
+        event.preventDefault();
+    },false);
+}
+
+//function to close all details when one is clicked
+const workContainers = document.querySelectorAll('.workContainer');
+function closeWorkContainer(event){
     //hide all project div before showing the selected project div
-    const workItems = document.querySelectorAll('.workItem');
-    for (const workItem of workItems){
-        workItem.hidden = true;
+    for (const workContainer of workContainers){
+        workContainer.open = false;
     }
-
-    //get the ID value of the targeted project div
-    const targetElementID = this.getAttribute('href').substr(1);
-    console.log(targetElementID);
-
-    //get the targeted project div and toggle the state of 'hidden' to show
-    const targetElement = document.getElementById(targetElementID);
-    targetElement.hidden = false;
-
-    //prevent the default action of navigating to the href value
-    event.preventDefault();
 }
 
 //add the event listeners to the titles of the works
-for(const anchor of worksTitle){
-    anchor.addEventListener('click', toggleWorkItem, false);
+for(workContainer of workContainers){
+    workContainer.addEventListener('click', closeWorkContainer, false);
 }
 
 
 //function to close the currently opened project div using button
 const closeButtons = document.querySelectorAll('.close');
-function closeWorkItem(event){
+function closeWorkContainer(event){
     //find the project div that is currently opened
-    const workItems = document.querySelectorAll('.workItem');
+    const workContainers = document.querySelectorAll('.workContainer');
     var currentElementID = "";
-    for (const workItem of workItems){
-        if(workItem.hidden == false){
-            currentElementID = workItem.getAttribute('id');
+    for (const workContainer of workContainers){
+        if(workContainer.open == true){
+            currentElementID = workContainer.getAttribute('id');
             console.log(currentElementID);
         }
     }
 
     //get the current project div and toggle the state of 'hidden' to close
     const currentElement = document.getElementById(currentElementID);
-    currentElement.hidden = true;
+    if(currentElement!=null){
+        currentElement.open = false;
+    }
 }
 
 //add the event listeners to the close buttons in all project div
 for(const button of closeButtons){
-    button.addEventListener('click', closeWorkItem, false);
+    button.addEventListener('click', closeWorkContainer, false);
 }
 
 
 //randomly apply styles to work-titles
+/*
 window.addEventListener('load', function() {
     //get the several random numbers within the length of worksTitle
     var arr = [];
@@ -69,3 +68,4 @@ window.addEventListener('load', function() {
     worksTitle[arr[2]].style.fontWeight = "bold";
     worksTitle[arr[3]].style.textDecoration = "underline";
 }, false);
+*/
